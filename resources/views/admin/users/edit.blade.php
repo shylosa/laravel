1bc81d119198cd1fec85c255652093bf3c4b761b
s.shylo <shylosa.mm@gmail.com>
2020-09-18 15:03:03 +0300

    User edit fix

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -23,12 +23,13 @@
 
     <!-- Main content -->
     <section class="content">
-	{{Form::open([
-		'route'	=>	['users.update', $user->id],
-		'method'	=>	'put',
-		'files'	=>	true
-	])}}
-      <!-- Default box -->      <div class="box">
+    {{ Form::open([
+      'route'	=>	['users.update', $user->id],
+      'method'	=>	'put',
+      'files'	=>	true
+    ]) }}
+    <!-- Default box -->
+      <div class="box">
         <div class="box-header with-border">
           <h3 class="box-title">Изменяем пользователя</h3>
           @include('admin.errors')
@@ -37,28 +38,31 @@
           <div class="col-md-6">
             <div class="form-group">
               <label for="exampleInputEmail1">Имя</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" name="name" placeholder="" value="{{$user->name}}">
+              <input type="text" class="form-control" id="exampleInputEmail1" name="name" placeholder=""
+                     value="{{ $user->name }}">
             </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">E-mail</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" name="email" placeholder="" value="{{$user->email}}">
+              <label for="email">E-mail</label>
+              <input type="text" class="form-control" id="email" name="email" placeholder=""
+                     value="{{ $user->email }}">
             </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">Пароль</label>
-              <input type="password" class="form-control" id="exampleInputEmail1" name="password" placeholder="">
+              <label for="password">Пароль</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="">
             </div>
             <div class="form-group">
-              <img src="{{$user->getImage()}}" alt="/img/no-image.png" width="200" class="img-responsive">
-                <button onclick="return confirm('Вы уверены?')" type="submit" name="update" value="delete-avatar" class="delete align-top" title="Удалить аватар">
-                  <i class="fa fa-remove fa-3x"></i>
-                </button>
+              <img src="{{ $user->getAvatar() }}" alt="/img/no-image.png" width="200" class="img-responsive">
+              <button onclick="return confirm('Вы уверены?')" type="submit" name="update" value="delete-avatar"
+                      class="delete align-top" title="Удалить аватар">
+                <i class="fa fa-remove fa-3x"></i>
+              </button>
             </div>
             <div class="form-group">
-                <div>
-                  <label for="exampleInputFile">Аватар</label>
-                </div>
-                <input type="file" name="avatar" id="exampleInputFile">
-                <p class="help-block">jpeg, jpg, png, bmp</p>
+              <div>
+                <label for="avatar">Аватар</label>
+              </div>
+              <input id="avatar" type="file" name="avatar">
+              <p class="help-block">jpeg, jpg, png, bmp</p>
             </div>
           </div>
         </div>
@@ -69,7 +73,7 @@
         <!-- /.box-footer-->
       </div>
       <!-- /.box -->
-	{{Form::close()}}
+      {{ Form::close() }}
     </section>
     <!-- /.content -->
   </div>
