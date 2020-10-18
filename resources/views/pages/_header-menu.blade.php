4f18f7f73629a445bfe22e28120ddba9b2bee351
serhii <shilkas2@gmail.com>
2020-10-18 19:09:03 +0300

    Fix edit project page

--- resources/views/pages/_header-menu.blade.php
+++ resources/views/pages/_header-menu.blade.php
@@ -1,1 +1,16 @@
-<?php
+<ul class="menu__list" onclick="document.getElementById('menu__toggle').checked=false">
+  <li><a class="menu__item" href="#about">{{ __('main.about') }}</a></li>
+  <li><a class="menu__item" href="#services">{{ __('main.services') }}</a></li>
+  <li><a class="menu__item" href="#portfolio">{{ __('main.portfolio') }}</a></li>
+  <li><a class="menu__item" href="#contacts">{{ __('main.contacts') }}</a></li>
+  @if (Auth::check() && Auth::user()->isAdmin())
+    <li>
+      <a class="menu__item" href="{{ route('admin') }}">{{ __('main.admin_page') }}</a>
+    </li>
+  @endif
+  @if (Auth::check())
+    <li>
+      <a class="menu__item" href="{{ route('logout') }}">{{ __('main.logout') }}</a>
+    </li>
+  @endif
+</ul>
