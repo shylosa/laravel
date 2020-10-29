af2dae27f96ea1427e5561aae856360e4ac6f389
s.shylo <shylosa.mm@gmail.com>
2020-10-29 11:34:36 +0200

    Added IE9 compatibility

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,10 +9,11 @@
   <!-- common css -->
   <link rel="stylesheet" href="/css/front-partial.css">
   <link rel="stylesheet" href="/css/front.css">
-  <!-- HTML5 shim and Respond.js IE9 support of HTML5 elements and media queries -->
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
-  <script src="assets/js/html5shiv.js"></script>
-  <script src="assets/js/respond.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
   <!-- Favicon -->
   <link rel="icon" type="image/png" href="/images/favicon.png">
